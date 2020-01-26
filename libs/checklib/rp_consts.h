7f471e9d4812e46b40f9e7360f83c34c2f214287
Alexander Larin <scalar438@hotmail.com>
2020-01-26 16:13:26 +0300

    Deprecate string constants for standard streams

--- libs/checklib/rp_consts.h
+++ libs/checklib/rp_consts.h
@@ -6,12 +6,20 @@ namespace checklib
 
 // ss - Standard Streams
 // Строковые константы с предопределенными названиями
-namespace ss
+namespace [[deprecated]] ss
 {
-const std::string Stdin = "stdin";
-const std::string Stdout = "stdout";
-const std::string Stderr = "stderr";
-const std::string Interactive = "interactive";
-}
+	const std::string Stdin       = "stdin";
+	const std::string Stdout      = "stdout";
+	const std::string Stderr      = "stderr";
+	const std::string Interactive = "interactive";
+} // namespace [[deprecated]]ss
 
-}
+enum class StandardStream
+{
+	Stdin,
+	Stdout,
+	Stderr,
+	Interactive
+};
+
+} // namespace checklib
