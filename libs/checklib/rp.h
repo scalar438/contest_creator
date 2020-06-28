22bf1f0730b5c24b8974af95723bb54a07789b47
Alexander Larin <scalar438@gmail.com>
2020-06-28 09:31:23 +0300

    Add missing rp.h

---
+++ libs/checklib/rp.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#pragma message("This is deprecated file, use process.hpp")
+
+#include "process.hpp"
+
+namespace checklib{
+
+
+typedef [[deprecated]] Process RestrictedProcess;
+
+}
